e8e6c0ffb834891071645ffe2f5b902818f43d2b
SoenkevL <soenke.v.l@web.de>
2023-09-19 10:49:32 +0200

    test settings for training on small subset

--- pytorch_training.py
+++ pytorch_training.py
@@ -58,12 +58,12 @@ valTransforms = monai.transforms.Compose(
 
 #create data dicitionaries and data loader to which monai transforms can be applied (transforms stored in Model_and_transforms.py)
 train_data_dict = [{"image":image_name,"label":label} for image_name, label in zip(train_image_IDs,train_image_labels)]
-val_data_dict = train_data_dict[-1000:]
-train_data_dict = train_data_dict[:-1000] #this can be optimized to shuffle beforehand for example
-train_ds = monai.data.CacheDataset(data=train_data_dict,transform=trainTransforms,cache_rate=0.1,num_workers=4,progress=True)
+val_data_dict = train_data_dict[-100:]
+train_data_dict = train_data_dict[:1000] #this can be optimized to shuffle beforehand for example
+train_ds = monai.data.CacheDataset(data=train_data_dict,transform=trainTransforms,cache_rate=0.5,num_workers=4,progress=True)
 val_ds = monai.data.CacheDataset(data=val_data_dict,transform=valTransforms,cache_rate=1,num_workers=4,progress=True)
-train_loader = monai.data.DataLoader(train_ds,batch_size=batch_size,shuffle=True,num_workers=0)
-val_loader = monai.data.DataLoader(val_ds,batch_size=batch_size,shuffle=True,num_workers=0)
+train_loader = monai.data.DataLoader(train_ds,batch_size=8,shuffle=True,num_workers=0)
+val_loader = monai.data.DataLoader(val_ds,batch_size=8,shuffle=True,num_workers=0)
 logging.log(0,'finished datasets')
 #intialize model, optimizer, loss
 model = MF.Net(n_outputclasses=N_train_classes).to(device=gpu)
