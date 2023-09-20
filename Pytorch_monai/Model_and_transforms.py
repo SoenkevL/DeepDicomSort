fixed an error in the model where the softmax was computed along the wrong dimension

Pytorch_monai/Model_and_transforms.py
@@ -47,7 +47,7 @@ class Net(nn.Module): #first version ignores the additional 4d parameter but sho
         x = F.relu(x)
         x = self.droput(x)
         x = self.fc2(x)
-        output = F.softmax(x,dim=0)
+        output = F.softmax(x,dim=1)
         return output
 
 

pytorch_training.py
@@ -151,7 +151,7 @@ def train(model, loss_function, train_dataloader, val_dataloader, optimizer, epo
     return train_loss, val_loss, bestModel
 
 #train
-trainloss, valloss, model = train(model,loss_function,train_loader,val_loader,optimizer,nb_epoch,device=gpu,val_freq=10)
+trainloss, valloss, model = train(model,loss_function,train_loader,val_loader,optimizer,nb_epoch,device=gpu,val_freq=2)
 
 #save model
 torch.save(model,os.path.join(output_folder,model_name+'.pt'))